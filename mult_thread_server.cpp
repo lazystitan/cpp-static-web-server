4529c898e89d6980bdc6cbdc53926a31878bcc54
Riton <ritonelion@outlook.com>
2019-10-13 16:42:04 +0800

    根据请求返回对应网页，没有找到返回404

--- mult_thread_server.cpp
+++ mult_thread_server.cpp
@@ -3,24 +3,63 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
+#include <regex>
 
 #include "request.h"
 #include "server.h"
 #include "thread_pool.h"
 
 #define BUFFER_LEN 4096
+#define MAX_REQUEST_TIMES 40
 
 using namespace std;
+
+void get_html(const char filename[], char buffer[], int n) {
+    ifstream fin(filename);
+
+    if (fin.is_open()) {
+        char head[] = "HTTP/1.1 200 OK \r\n"
+                      "\r\n";
+        for (int j = 0; j < strlen(head); ++j) {
+            buffer[j] = head[j];
+        }
+        char ch;
+        for (int i = strlen(head); (i < n) && (fin.get(ch)); ++i) {
+            buffer[i] = ch;
+        }
+    } else {
+        char head[] = "HTTP/1.1 404 Not Found \r\n"
+                      "\r\n";
+        for (int j = 0; j < strlen(head); ++j) {
+            buffer[j] = head[j];
+        }
+        ifstream fin_not_found("../404.html");
+        char ch;
+        for (int i = strlen(head); (i < n) && (fin_not_found.get(ch)); ++i) {
+            buffer[i] = ch;
+        }
+        fin_not_found.close();
+    }
+    fin.close();
+}
 
 void handle_request(Request &request) {
     char buffer[BUFFER_LEN];
     bzero(buffer, BUFFER_LEN);
     request.read(buffer, BUFFER_LEN - 1);
     cout << buffer << endl;
-    char content[] = "HTTP/1.1 200 OK \r\n"
-                     "\r\n"
-                     "<p>Hello, this is a multiple thread web server.<p>";
+    char content[4096]{'\0'};
+
+    char filename[20] = "..";
+    int start = 4, index = 2;
+    for (char ch = buffer[start]; ch != ' '; ch = buffer[++start]) {
+        filename[index++] = ch;
+    }
+    filename[index] = '\0';
+
+    get_html(filename, content, 4095);
     request.send(content, strlen(content), 0);
     request.close();
 }
@@ -32,7 +71,8 @@ int main() {
     Channel<Work> channel;
     ThreadPool threadPool(5, channel);
 
-    for (int i = 0; i < 40; ++i) {
+
+    for (int i = 0; i < MAX_REQUEST_TIMES; ++i) {
         Request request(tcpServer.accept());
         Work work;
         work._request = &request;
